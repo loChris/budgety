0e670ac39c6e467d64810774d7ff30c7d50b6918
loChris <44450518+loChris@users.noreply.github.com>
2019-07-06 10:21:31 -0500

    Added input for DOM manipulation

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
-var budgetController = (function(){
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+// BUDGET CONTROLLER
+var budgetController = (function() {
+    // some code
 })();
+//END BUDGET CONTROLLER
 
-var UIController = (function(){
+
+// UI CONTROLLER
+var UIController = (function() {
     //some code
-})();
-
-var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(5);
-
-    return {
-        anotherPublicMethod: function(){
-            console.log(z);
-        }
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
     }
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //will be either income or expenses
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+})();
+//END UI CONTROLLER
+
+
+// GLOBAL CONTROLLER
+var controller = (function(budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
+    var ctrlAddItem = function() {
+         // 1. Get the filled input data
+         var input = UICtrl.getInput();
+         console.log(input);
+
+        // 2. Add item to budget controller
+
+        // 3. Add the new item to the UI
+
+        // 4. Calculate the budget
+
+        // 5 Display the budget in the UI
+
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(e) {
+        if(e.keyCode === 13 || e.which === 13){
+            ctrlAddItem();
+        }
+    });
 })(budgetController, UIController);
+//END GLOBAL CONTROLLER
